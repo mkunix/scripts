fff7883bcb8b6e30d9ec59e5034a2de4c01d543e
mkunix <shanmuga0902@gmail.com>
2020-02-22 13:41:41 -0500

    just commit

--- shell/sysinfo.sh
+++ shell/sysinfo.sh
@@ -8,7 +8,8 @@ system_info() {
   lsb_release -drc
   echo
   echo "#### Processor Informaton"
-  echo $(grep -wc processor /proc/cpuinfo)
+  echo No. of processors\: $(grep -wc processor /proc/cpuinfo)
+  echo $(grep -m1  "model name" /proc/cpuinfo)
 }
 
 system_info
