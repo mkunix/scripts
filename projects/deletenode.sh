96e7de6d72ffd2d6414f9f6fcc14833299ba51f8
mkunix <shanmuga0902@gmail.com>
2020-03-07 16:10:03 -0500

    more

--- projects/deletenode.sh
+++ projects/deletenode.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 #Delete Chef Node
-
 NODE=$1
 SHORTY=$(echo $NODE|awk -F"." '{print $1}')
 
-knife node show ${NODE} --format json > /tmp/${SHORTY}.json > /dev/null
-knife node delete ${NODE} -y > /dev/null
-sleep 2
-knife client delete ${NODE} -y /dev/null
-sleep 2
-knife node from file /tmp/${SHORTY}.json  >&1
+KNIFE_CMDS() {
 
+NODE_FILE="/tmp/${SHORTY}.json"
+NODE_SAVE="knife node show ${NODE} --format json"
+$NODE_SAVE > ${NODE_FILE}
+NODE_LINES=$(wc -l ${NODE_FILE})
+
+[[ -f /tmp/${SHORTY}.json ]] && [[ ${NODE_LINES -gt 5} ]] && echo "Saved file verified to be non-empty file"
+
+knife node delete ${NODE} -y
+sleep 2
+knife client delete ${NODE} -y
+sleep 2
+knife node from file /tmp/${SHORTY}.json
+}
+
+POST_KNIFE() {
+#Post Knife tasks on Cliet
 read -p "Press 1 to reboot, 2 to run chef client, q to quit: " fix_input
-
 if [[ ${fix_input} == "1" ]]
 then
   echo "Reboot host commands: "
@@ -25,5 +34,25 @@ elif [[ ${fix_input} == "q" ]]
 then
   echo "Quitting..."
   exit
-  #statements
+fi
+
+}
+
+USAGE() {
+echo "USAGE: `basename $0` FQDN [File Name]"
+echo "Example:"
+echo "`basename $0` term00x-ops-23.portal.webmd.com"
+echo "`basename $0` /tmp/hostslist"
+}
+
+#Main Program
+if [[ $# -eq "0" ]]
+then USAGE
+fi
+
+if [[ -f $1 ]]
+then
+  echo "TODO: work for file "
+elif [[ $1 =~ ^[a-zA-Z].*.portal.webmd.com ]]
+then KNIFE_CMDS
 fi
